9ab82e37c056fce2a53bd6f770792b68f49f163e
Tomas Madeja <tomas.madeja@protonmail.com>
2022-09-06 22:04:46 +0200

    WIP

--- src/lib/components/FilterBar.svelte
+++ src/lib/components/FilterBar.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
     // export let tags: string[]; 
     let search: string = ""
+
+    function onSelect() {
+
+    }
+
+    function onDeselect() {
+
+    }
 </script>
 
 <input bind:value={search}>
+<div>
+    <button on:select={onSelect on:select}></button>
+</div>
